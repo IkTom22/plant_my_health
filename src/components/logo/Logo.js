da436df0515688fa9d1d17660e672b88a8484e7c
Ico Tominaga <icotominaga@gmail.com>
2022-03-12 16:38:55 +1300

    logo text curv css is added

--- src/components/logo/Logo.js
+++ src/components/logo/Logo.js
@@ -3,36 +3,40 @@ import styled from "styled-components"
 import Logo_svg from "../../assets/images/logo_3.svg"
 
 const Logo_container = styled.div`
-  stroke: var(--black);
-  height: 9rem;
-  width: 9rem;
+  height: 10rem;
+  width: 10rem;
   background-color: var(--white);
   border-radius: 50%;
   border: var(--white) 10px solid;
   box-shadow: var(--shadow-2);
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
 `
 export const Logo = () => {
   return (
-    <Logo_container className="d-flex flex-column justify-content-center align-items-center">
-      <Logo_svg />
-      <div className="logo_letters">
+    <Logo_container>
+      <div className="logo-letters logo-letters_plant">
         <span>P</span>
-        <span>l</span>
-        <span>a</span>
-        <span>n</span>
-        <span>t</span>
-        <span></span>
+        <span>L</span>
+        <span>A</span>
+        <span>N</span>
+        <span>T</span>
+      </div>
+      <div classnme="logo-letters_my">
         <span>M</span>
-        <span>y</span>
-        <span></span>
-        <span>H</span>
-        <span>e</span>
-        <span>a</span>
-        <span>l</span>
-        <span>t</span>
         <span>
-          <h3></h3>
+          <Logo_svg />
         </span>
+      </div>
+      <div className="logo-letters logo-letters_health">
+        <span>H</span>
+        <span>E</span>
+        <span>A</span>
+        <span>L</span>
+        <span>T</span>
+        <span>H</span>
       </div>
     </Logo_container>
   )
